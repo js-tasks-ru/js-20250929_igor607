4dbf9587cbde36c1e19e89c9992572784d1385ee
Zubkov Igor <zubkovigor@mail.ru>
2025-10-03 18:20:49 +0200

    remove comment code

--- prittier.config.js
+++ prittier.config.js
@@ -1,10 +1,1 @@
-// {
-//   "useTabs": false,
-//   "tabWidth": 2,
-//   "singleQuote": true,
-//   "semi": true,
-//   "printWidth": 120,
-//   "trailingComma": "all",
-//   "proseWrap": "never"
-// }
 module.exports = require("@quave/eslint-config-quave/prettier.config");
