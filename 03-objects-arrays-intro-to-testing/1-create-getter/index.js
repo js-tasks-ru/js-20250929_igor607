e9c5740494a512cf4b8d6448d51bcd3c470cd5e6
Zubkov Igor <zubkovigor@mail.ru>
2025-10-03 12:52:37 +0200

    task 3 compete

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const parts = path.split(".");
 
+  return function (obj) {
+    let current = obj;
+
+    for (const part of parts) {
+      if (current === undefined || current === null) {
+        return undefined;
+      }
+      if (!Object.hasOwn(current, part)) {
+        return undefined;
+      }
+      current = current[part];
+    }
+
+    return current;
+  };
 }
