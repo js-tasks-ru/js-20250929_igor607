da1fc3a25d6333113699e8b621f8c1038b4c6ad8
Zubkov Igor <zubkovigor@mail.ru>
2025-10-15 12:36:02 +0200

    NotificationMessage fnc implemented

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,55 @@
 export default class NotificationMessage {
+  static activeNotification;
+  element;
+  timerId;
 
+  constructor(message = "", { type = "success", duration = 2000 } = {}) {
+    this.type = type;
+    this.message = message;
+    this.duration = duration;
+
+    this.createElement();
+  }
+
+  createNotificationTemplate() {
+    return `
+    <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>`;
+  }
+
+  createElement() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.createNotificationTemplate();
+    this.element = wrapper.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+
+    NotificationMessage.activeNotification = this;
+    target.appendChild(this.element);
+
+    setTimeout(() => this.destroy(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+      this.timerId = null;
+    }
+    this.remove();
+  }
 }
